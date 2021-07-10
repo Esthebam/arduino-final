c292977f4652874316eb04b8f4e1aa0a4e3112d0
Esteban Matas <estebanmatas13@gmail.com>
2021-07-10 00:05:45 -0300

    prueba heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = app.listen(port, () => {
 
 const io = require('socket.io')(server);
 
-io.on('connection', (socket) => {
+io.of('/').on('connection', (socket) => {
 
   console.log('New connection: ' + socket.id);
 
