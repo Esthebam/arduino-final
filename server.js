5ed2cbb2e3f4d85df0613cc5f212115d146ae421
Esteban Matas <estebanmatas13@gmail.com>
2021-07-10 09:02:31 -0300

    prueba heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-//const port = process.env.PORT || 4000;
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
